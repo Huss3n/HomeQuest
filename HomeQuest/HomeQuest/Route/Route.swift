e5b5abdb8241754becc5954648c20e3a67dd12c2
Hussein <aisackhussein@gmail.com>
2024-07-15 13:32:58 +0300

    Complete user onboarding view

---
+++ HomeQuest/HomeQuest/Route/Route.swift
@@ -0,0 +1,14 @@
+//
+//  Route.swift
+//  HomeQuest
+//
+//  Created by Muktar Aisak on 15/07/2024.
+//
+
+import Foundation
+import SwiftUI
+
+enum Route: Hashable {
+    case login
+    case home
+}
