8479e1b4f872de7385706f15683d37e9dad9958b
Hussein <aisackhussein@gmail.com>
2024-07-18 15:00:15 +0300

    Add Map kit ans property locations

---
+++ HomeQuest/HomeQuest/Core/MapView/Model/PropertyLocations.swift
@@ -0,0 +1,8 @@
+//
+//  PropertyLocations.swift
+//  HomeQuest
+//
+//  Created by Muktar Aisak on 18/07/2024.
+//
+
+import Foundation
---
+++ HomeQuest/HomeQuest/Core/MapView/ViewModel/MapViewmodel.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewmodel.swift
+//  HomeQuest
+//
+//  Created by Muktar Aisak on 18/07/2024.
+//
+
+import Foundation
